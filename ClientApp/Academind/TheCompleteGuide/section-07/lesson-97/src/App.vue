2cff15962bc228166305e2e672b02d25d1e2850d
Phil Boyd <phil.boyd@live.com>
2019-09-17 12:15:16 -0400

    WIP - created lesson-97

---
+++ ClientApp/Academind/TheCompleteGuide/section-07/lesson-97/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <h1>Hello world</h1>
+      <p>Server Status: {{status}}</p>
+      <hr>
+      <app-home-component></app-home-component>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            status: 'Critical!',
+        }
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
