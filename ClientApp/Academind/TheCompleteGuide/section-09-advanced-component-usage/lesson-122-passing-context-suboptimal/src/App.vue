2d9b4f8ddb338771b2c7e7382172a4469e0ea727
Phil Boyd <phil.boyd@live.com>
2019-09-21 20:22:02 -0400

    Lesson 122 initialized

---
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-122-passing-context-suboptimal/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component({
+        components: {
+        },
+    })
+    export default class App extends Vue { }
+</script>
+
+<style lang="scss">
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+</style>
