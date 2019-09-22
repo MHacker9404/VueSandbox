4872ed9c308f1bde8640e7fa72c9abd8daae7b78
Phil Boyd <phil.boyd@live.com>
2019-09-22 08:17:55 -0400

    Lesson 126 - name slots: initialized

---
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-126-named-slots/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <app-quote>
+            <h2>{{title}}</h2>
+            <p>{{quote}}</p>
+        </app-quote>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import Quote from './components/Quote.vue';
+
+    @Component({
+        components: {
+            'app-quote': Quote,
+        },
+    })
+    export default class App extends Vue {
+        private quote: string = `It's a wonderful life!`;
+        private title: string = `The Quote`;
+     }
+</script>
+
+<style scoped lang="scss">
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+    h2 {
+        color: green;
+    }
+</style>
