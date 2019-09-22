129f727932d3b2eea4f2e7b97707b3f5b08e68e4
Phil Boyd <phil.boyd@live.com>
2019-09-22 15:34:47 -0400

    Lesson 129 complete - intro to dynamic components

---
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-129-dynamic-components/src/components/Author.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='author'>
+        <h3>The Author</h3>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue, Prop } from 'vue-property-decorator';
+
+    @Component
+    export default class Author extends Vue { }
+</script>
+
+<style scoped lang="scss">
+    .author {
+        border: 2px dotted red;
+        box-shadow: 2px 2px 4px lightblue;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
---
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-129-dynamic-components/src/components/New.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='new'>
+        <h3>New Quote</h3>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue, Prop } from 'vue-property-decorator';
+
+    @Component
+    export default class New extends Vue { }
+</script>
+
+<style scoped lang="scss">
+    .new {
+        border: 2px dashed green;
+        box-shadow: 2px 2px 4px lightgray;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
