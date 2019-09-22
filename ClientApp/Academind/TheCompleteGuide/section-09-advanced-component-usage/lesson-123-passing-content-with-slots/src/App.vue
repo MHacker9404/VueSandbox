f3e46f0a7b914f47599b4b0cc114fcf269d70235
Phil Boyd <phil.boyd@live.com>
2019-09-22 08:04:48 -0400

    Lesson 123 complete - intial slots

---
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-123-passing-content-with-slots/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <app-quote>
+            <h2>The Quote</h2>
+            <p>{{quote}}</p>
+        </app-quote>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import Quote from './components/Quote.vue';
+
+    @Component({
+        components: {
+            'app-quote': Quote,
+        },
+    })
+    export default class App extends Vue {
+        private quote: string = `It's a wonderful life!`;
+     }
+</script>
+
+<style lang="scss">
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+</style>
---
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-123-passing-content-with-slots/src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class='component quote'>
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue, Prop } from 'vue-property-decorator';
+
+    @Component
+    export default class Quote extends Vue {
+     }
+</script>
+
+<style scoped lang="scss">
+    .quote {
+        border: 1px solid lightgray;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
