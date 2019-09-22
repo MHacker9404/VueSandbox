bd3cebb8984689957adb6b9ca6b14df16cc21b89
Phil Boyd <phil.boyd@live.com>
2019-09-22 08:14:45 -0400

    Lesson 125 complete - styling slots

--- ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-123-passing-content-with-slots/src/App.vue
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-123-passing-content-with-slots/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <app-quote>
-            <h2>The Quote</h2>
+            <h2>{{title}}</h2>
             <p>{{quote}}</p>
         </app-quote>
     </div>
@@ -18,12 +18,16 @@
     })
     export default class App extends Vue {
         private quote: string = `It's a wonderful life!`;
+        private title: string = `The Quote`;
      }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
     div.component {
         border: 1px solid black;
         padding: 30px;
+    }
+    h2 {
+        color: green;
     }
 </style>
--- ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-123-passing-content-with-slots/src/components/Quote.vue
+++ ClientApp/Academind/TheCompleteGuide/section-09-advanced-component-usage/lesson-123-passing-content-with-slots/src/components/Quote.vue
@@ -20,4 +20,7 @@
         margin: 30px auto;
         text-align: center;
     }
+    h2 {
+        color: red;
+    }
 </style>
