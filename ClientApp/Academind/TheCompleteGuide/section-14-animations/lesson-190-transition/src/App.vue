a0443bcac87dbba72d2971433392f69e9f00ac1f
Phil Boyd <phil.boyd@live.com>
2019-10-02 12:20:14 -0400

    WIP

---
+++ ClientApp/Academind/TheCompleteGuide/section-14-animations/lesson-190-transition/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+     <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1> Animations </h1>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: { },
+    directives:{ },
+})
+export default class App extends Vue {
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
