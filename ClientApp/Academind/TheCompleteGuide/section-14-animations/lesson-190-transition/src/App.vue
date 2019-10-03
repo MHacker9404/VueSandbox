d06bf4d0ee09b4f03fb88289bf73f0ac63865221
Phil Boyd <phil.boyd@live.com>
2019-10-03 09:14:47 -0400

    Lesson 193 complete - using transition element

--- ClientApp/Academind/TheCompleteGuide/section-14-animations/lesson-190-transition/src/App.vue
+++ ClientApp/Academind/TheCompleteGuide/section-14-animations/lesson-190-transition/src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="app">
-     <div class="container">
+<div id="app">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1> Animations </h1>
+                <hr>
+                <button class="btn btn-primary" @click='show = !show'>Show Alert</button>
+                <br />
+                <br />
+                <transition name='fade'>
+                    <div class="alert alert-info" v-if='show'>This is some info</div>
+                </transition>
             </div>
         </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
@@ -18,6 +25,7 @@ import { Component, Vue } from 'vue-property-decorator';
     directives:{ },
 })
 export default class App extends Vue {
+    private show: boolean = false;
 }
 </script>
 
@@ -29,5 +37,18 @@ export default class App extends Vue {
   text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter {
+    opacity: 0;
+}
+.fade-enter-active {
+    transition: opacity 1.5s;
+}
+.fade-leave {}
+
+.fade-leave-active {
+    transition: opacity 1.5s;
+    opacity: 0;
 }
 </style>
