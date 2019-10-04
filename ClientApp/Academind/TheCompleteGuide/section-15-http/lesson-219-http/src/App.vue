f7552c7a66aafe5ee5b8cb5b74cf32d1ea096e0a
Phil Boyd <phil.boyd@live.com>
2019-10-03 21:27:28 -0400

    Lesson 220 complete - firebase setup and POST data

---
+++ ClientApp/Academind/TheCompleteGuide/section-15-http/lesson-219-http/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="app">
+    <div class="container-fluid">
+        <div class="row">
+            <!--<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">-->
+            <div class="col">
+                <h1 class='text-center'>HTTP</h1>
+                <div class="form-group">
+                    <label for='userName'>Username</label>
+                    <input type="text" id='userName' class="form-control" v-model='user.userName'>
+                </div>
+                <div class="form-group">
+                    <label for='passWord'>Password</label>
+                    <input type="password" id='passWord' class="form-control" v-model='user.password'>
+                </div>
+                <button class="btn btn-primary" @click='submit'>Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: { },
+})
+export default class App extends Vue {
+    private user = {
+        userName: '',
+        password: '',
+    };
+    private submit(): void {
+        //  https://vue-http-37f3f.firebaseio.com/
+        this.$http.post(`https://vue-http-37f3f.firebaseio.com/data.json`, this.user)
+            .then(res => console.log(res), error => console.log(error));
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
