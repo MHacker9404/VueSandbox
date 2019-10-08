042c721bec4a4ba8fc042f9ced6ec5fbe15e5b32
Phil Boyd <phil.boyd@live.com>
2019-10-07 21:13:49 -0400

    Lesson 223 complete - initial routing setup

---
+++ ClientApp/Academind/TheCompleteGuide/section-16-routing/lesson-223-routes/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <div class="container-fluid">
+            <div class="row">
+                <!--<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">-->
+                <div class="col">
+                    <h1>Routing</h1>
+                    <hr />
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {},
+})
+export default class App extends Vue {}
+</script>
+
+<style scoped lang="scss">
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ ClientApp/Academind/TheCompleteGuide/section-16-routing/lesson-223-routes/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>The Home Page</h1>
+        <hr />
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aut autem, blanditiis consequuntur dolore
+            excepturi laborum maiores minima nihil non nulla obcaecati quas quibusdam quod sed suscipit vero voluptatem.
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {},
+})
+export default class Home extends Vue {}
+</script>
